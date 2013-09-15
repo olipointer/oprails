7bfbcb6af162f78e0af78e0f90ad858f072ff7e8
Oli Pointer <olipointer@gmail.com>
2013-09-15 10:49:10 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oprails::Application.config.secret_token = '8a99726756a7e4b658e55a4ed4e82b5634e162dcaed0532fb5da8c9c4f8482dc5912c93cefda5f17dbd0065fcf750b6e3190b11f7f8975cff853a382d0dcd386'
